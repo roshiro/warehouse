001c2aa892d9a4806646c3dd12e97ef703d12e90
Rafael Oshiro <rafael.oshiro@gmail.com>
2016-09-16 19:31:10 +0100

    Spec for request dispatcher

--- spec/lib/request_dispatcher_spec.rb
+++ spec/lib/request_dispatcher_spec.rb
@@ -3,8 +3,12 @@ require 'spec_helper.rb'
 describe 'RequestDispatcher' do
   include Rack::Test::Methods
 
-  it 'includes HTTParty module' do
-
+  let(:response) { double("Response") }
+  let(:path) { "path" }
+  before :each do
+    allow(RequestDispatcher).to receive(:get)
+    allow(RequestDispatcher).to receive(:get).and_return response
+    RequestDispatcher.class_variable_set :@@cache, nil
   end
 
   it 'has correct base URI' do
@@ -12,18 +16,21 @@ describe 'RequestDispatcher' do
   end
 
   describe '.get_and_cache' do
-    before :each do
-      allow(described_class).to receive(:get)
-    end
-
     it 'caches the first call' do
-      
+      RequestDispatcher.get_and_cache(path)
+      expect(RequestDispatcher.cache).to eq({ path => response })
     end
 
-    it 'returns the cached version'
+    it 'returns the cached version' do
+      RequestDispatcher.cache[path] = "some_other_content"
+      expect(RequestDispatcher.get_and_cache(path)).to eq("some_other_content" )
+    end
   end
 
   describe '.cache' do
-
+    it 'caches the response of the request with correct key' do
+      RequestDispatcher.get_and_cache(path)
+      expect(RequestDispatcher.cache).to eq({"path"=>"some_other_content"})
+    end
   end
 end
